c44d39097c51fa72bb43002f6e913d25b6fdc6fe
jeongseungwoo1 <jsw11062004@gmail.com>
2025-07-27 20:04:28 +0900

    초침을 깨물다

--- frontend/src/layout/Header1.jsx
+++ frontend/src/layout/Header1.jsx
@@ -4,13 +4,13 @@ function HeaderTab1() {
   };
 
   return (
-    <header className="w-full bg-white flex items-center px-2 sm:px-3 py-1 sm:py-1.5 mb-8 shadow-sm">
-      <div className="flex items-center gap-1.5">
+    <header className="w-full bg-white flex items-center px-5 sm:px-3 py-1 sm:py-1.5 mb-8 border-b border-gray-200 border-[1px]">
+      <div className="flex items-center gap-2">
         <img
-          src="/3034007-slide-s-3-whats-the-difference-between-a-logo-and-a-symbol.ico" // 노란색 배경에 + 로고 (예시)
+          src="/icon.png" // 노란색 배경에 + 로고 (예시)
           alt="로고"
-          width={55}   // 사이즈를 48px로 늘림
-          height={55}  // 사이즈를 48px로 늘림
+          width={40}   // 사이즈를 48px로 늘림
+          height={40}  // 사이즈를 48px로 늘림
           className="rounded cursor-pointer"
           onClick={handleLogoClick}
         />
